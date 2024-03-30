758a6f8a3eaaf134317cfca3b0a5596a9c9e2c19
David Acuña <89756491+fiito720@users.noreply.github.com>
2024-03-29 21:20:14 -0600

    Revert "nada importante"
    This reverts commit 54ff313cf9e8edac2a81620d9b2e224320070f94.

--- Setion_AP/app/src/main/java/com/example/setion_ap/Adapters/AdapterGP_ConsultaProyectos.kt
+++ Setion_AP/app/src/main/java/com/example/setion_ap/Adapters/AdapterGP_ConsultaProyectos.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.setion_ap.R
 
@@ -12,7 +13,6 @@ class AdapterGP_ConsultaProyectos(context: Context, resource: Int, objects: List
     ArrayAdapter<String>(context, resource, objects) {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var convertView = convertView
-        convertView
 
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.__item_cp_consulta_proyectos, parent, false)
