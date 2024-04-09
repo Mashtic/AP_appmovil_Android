b942734d4d979b672aba71967688995103965c1f
David Acuña <89756491+fiito720@users.noreply.github.com>
2024-04-08 23:38:12 -0600

    /

--- Setion_AP/app/src/main/java/com/example/setion_ap/APP/Main_GestionColaboradores.kt
+++ Setion_AP/app/src/main/java/com/example/setion_ap/APP/Main_GestionColaboradores.kt
@@ -1,6 +1,7 @@
 package com.example.setion_ap.APP
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.setion_ap.R
 
 class Main_GestionColaboradores : AppCompatActivity() {
+    private lateinit var btnAtras:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
